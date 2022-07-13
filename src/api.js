a9e773c76a2c9bb86c9e10c54af92180d32eb285
Thiago <bodnarthiago@gmail.com>
2022-07-13 15:40:20 -0300

    chore: intala a lib assync handler, cria middleware de erro, pasta errors com NotFoundError, rotas, e validators para rodar schemas do joi

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const usersRoutes = require('./routers/usersRoutes');
 
 // ...
 
@@ -8,6 +9,25 @@ app.use(express.json());
 
 // ...
 
+app.use('/', usersRoutes);
+
+app.use((err, _req, res, _next) => {
+  const { name, message } = err;
+  switch (name) {
+    case 'ValidationError':
+      res.status(400).json({ message });      
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'UnprocessableError':
+      res.status(422).json({ message });
+      break;
+    default:
+      console.warn(err); res.sendStatus(500);
+      break;
+  }
+});
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
---
+++ src/routers/usersRoutes.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const asyncHandler = require('express-async-handler');
+
+const usersController = require('../controllers/userController');
+
+const usersRouter = Router();
+
+// router.get('/', asyncHandler(usersController.list));
+usersRouter.post('/login', asyncHandler(usersController.create));
+// router.get('/:id', usersController.findById);
+
+module.exports = usersRouter;
