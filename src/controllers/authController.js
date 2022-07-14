a56b08dd9c4ad6fb88dc08c159a52e444c0c1292
Thiago <bodnarthiago@gmail.com>
2022-07-14 17:14:43 -0300

    feat: middleware de autolizaçao criado

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,19 @@
 const authService = require('../services/authService');
+const jwtService = require('../services/jwtService');
 
 const authController = {
   async login(req, res) {
     authService.validateBody(req.body);
     const token = await authService.login(req.body);
     res.status(200).json(token); 
+  },
+
+  validateToken(req, res, next) {
+    const { authorization } = req.headers;
+
+    jwtService.validateToken(authorization);
+
+    next();
   },
 };
 
