473f965e3062f7e7a4cf216a69f08745a8624177
Thiago <bodnarthiago@gmail.com>
2022-07-15 14:45:52 -0300

    feat: model category criado

---
+++ src/database/models/category.js
@@ -0,0 +1,18 @@
+const createCategory = (sequelize, DataTypes) => {
+  const Category = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+
+  return Category;
+};
+
+module.exports = createCategory;
